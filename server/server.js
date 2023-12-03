e93e1980fb62829dc9d67bab5ed09b9409eb0b9d
sota <sota1210nittc@gmail.com>
2023-12-03 20:54:40 +0900

    駅名,コードでの検索と各駅の状態の取得するクエリを追加

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
+const fs = require("fs");
 const express = require("express");
+const sqlite3 = require("sqlite3");
+
+const db_path = "./station.db";
+if(!fs.existsSync(db_path)){
+  console.error(`Error: ${db_path} does not exist`);
+  process.exit(1);
+}
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const db = new sqlite3.Database(db_path);
 
 app.get("/", (req, res) => {
   res.end("OK");
@@ -9,6 +19,66 @@ app.get("/", (req, res) => {
 
 app.get("/api", (req, res) => {
   res.json({ res: "OK" });
+});
+
+app.get("/api/station/:stationCode", (req, res) => {
+  const code = req.params.stationCode;
+  db.get(`
+      SELECT * FROM StationCodes
+      INNER JOIN StationNames
+        ON StationCodes.stationGroupCode = StationNames.stationGroupCode
+          AND StationCodes.stationCode = ?
+    `,
+    code,
+    (err, data) => {
+      if(err) console.error(err);
+      res.json(data);
+    }
+  );
+});
+
+app.get("/api/searchStationName", (req, res) => {
+  const name = req.query.name;
+  if(name === undefined){
+    res.json({});
+    return;
+  }
+  db.all(`
+      SELECT * FROM StationCodes
+      INNER JOIN StationNames
+        ON StationCodes.stationGroupCode = StationNames.stationGroupCode
+          AND StationNames.stationName = ?
+    `,
+    name,
+    (err, data) => {
+      if(err) console.error(err);
+      res.json(data);
+    }
+  );
+});
+
+app.get("/api/stationState/:stationCode", (req, res) => {
+  const code = req.params.stationCode;
+  db.get(
+    "SELECT * FROM StationState WHERE stationCode = ?",
+    code,
+    (err, data) => {
+      if(err) console.error(err);
+      res.json(data);
+    }
+  );
+});
+
+app.get("/api/stationGroupState/:stationGroupCode", (req, res) => {
+  const code = req.params.stationGroupCode;
+  db.get(
+    "SELECT * FROM StationGroupState WHERE stationGroupCode = ?",
+    code,
+    (err, data) => {
+      if(err) console.error(err);
+      res.json(data);
+    }
+  );
 });
 
 app.listen(PORT);
