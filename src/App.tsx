85f0555781a9ff711fd91daf8676c0da05634a3a
sota <sota1210nittc@gmail.com>
2023-12-06 18:01:05 +0900

    Headerファイルの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Header from "./Header";
 import StationList from "./StationList";
 import StationInfo from "./StationInfo";
 import SearchStation from "./SearchStation";
@@ -10,8 +11,8 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <p>header</p>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<p>hello</p>} />
           <Route path="/stationList" element={<StationList />} />
