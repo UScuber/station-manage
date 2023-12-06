c2140c48e4b6fc3503dae9297476e8ddec1ccd26
sota <sota1210nittc@gmail.com>
2023-12-07 00:55:24 +0900

    Material UIとヘッダの追加

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,68 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Box,
+  CssBaseline,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const navItems: Array<{ text: string, url: string }> = [
+  { text: "Top", url: "/" },
+  { text: "List", url: "/stationList" },
+  { text: "Search", url: "/searchStation" },
+];
+
 
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
   return (
-    <header>
-      <p>Header</p>
-    </header>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            StationManage
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block", textAlign: "center" } }}>
+            <List component="nav" sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              {navItems.map((item) => (
+                <ListItem key={item.text} disablePadding>
+                  <ListItemButton component={Link} to={item.url} sx={{ textAlign: 'center' }}>
+                    <ListItemText primary={item.text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
