e258e28a9034fdec81e8e1b3b2427ca32e4ee789
sota <sota1210nittc@gmail.com>
2023-12-07 15:11:02 +0900

    URLの設定とnavbarの作成

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,6 +6,9 @@ import {
   IconButton,
   Typography,
   Box,
+  Button,
+  Divider,
+  Drawer,
   List,
   ListItem,
   ListItemButton,
@@ -19,6 +22,7 @@ const navItems: Array<{ text: string, url: string }> = [
   { text: "Search", url: "/searchStation" },
 ];
 
+const drawerWidth = 240;
 
 const Header = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -29,7 +33,7 @@ const Header = () => {
 
   return (
     <>
-      <AppBar component="nav" position="static">
+      <AppBar position="static">
         <Toolbar>
           <IconButton
             color="inherit"
@@ -45,7 +49,14 @@ const Header = () => {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            StationManage
+            <Button
+              component={Link}
+              color="inherit"
+              to="/"
+              sx={{ fontSize: "20px", color: "#fff", textTransform: "none" }}
+            >
+              Station Manage
+            </Button>
           </Typography>
           <Box sx={{ display: { xs: "none", sm: "block", textAlign: "center" } }}>
             <List component="nav" sx={{ display: 'flex', justifyContent: 'flex-start' }}>
@@ -60,6 +71,44 @@ const Header = () => {
           </Box>
         </Toolbar>
       </AppBar>
+      <nav>
+        <Drawer
+          container={window.document.body}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+              <Button
+                component={Link}
+                color="inherit"
+                to="/"
+                sx={{ fontSize: "20px", textTransform: "none" }}
+              >
+                Station Manage
+              </Button>
+            </Typography>
+            <Divider />
+            <List>
+              {navItems.map((item) => (
+                <ListItem key={item.text} disablePadding>
+                  <ListItemButton component={Link} to={item.url} sx={{ textAlign: 'center' }}>
+                    <ListItemText primary={item.text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+      </nav>
     </>
   );
 };
