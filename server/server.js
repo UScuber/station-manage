89add081a45962bc259928c223dff1e787b77705
sota <sota1210nittc@gmail.com>
2023-12-07 17:53:50 +0900

    最も近い駅を求めるクエリの追加

--- server/server.js
+++ server/server.js
@@ -57,6 +57,38 @@ app.get("/api/searchStationName", (req, res) => {
   );
 });
 
+app.get("/api/searchNearestStationGroup", (req, res) => {
+  const lat = req.query.lat;
+  const lng = req.query.lng;
+  if(lat === undefined || lng == undefined){
+    res.json({});
+    return;
+  }
+  db.all(`
+      SELECT * FROM StationNames
+      WHERE (
+        6371 * ACOS(
+          COS(RADIANS(?)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS(?))
+          + SIN(RADIANS(?)) * SIN(RADIANS(latitude))
+        )
+      ) = (
+        SELECT MIN(
+          6371 * ACOS(
+            COS(RADIANS(?)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS(?))
+            + SIN(RADIANS(?)) * SIN(RADIANS(latitude))
+          )
+        )
+        FROM StationNames
+      )
+    `,
+    lat,lng,lat, lat,lng,lat,
+    (err, data) => {
+      if(err) console.error(err);
+      res.json(data);
+    }
+  );
+});
+
 app.get("/api/stationState/:stationCode", (req, res) => {
   const code = req.params.stationCode;
   db.get(
