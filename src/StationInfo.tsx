1fcf953a3a76b77d801be883fa5a9efd0943e90b
sota <sota1210nittc@gmail.com>
2023-12-08 12:09:10 +0900

    loading画面の更新

--- src/StationInfo.tsx
+++ src/StationInfo.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router-dom";
 import { useStationInfo } from "./Api";
+import { Box, CircularProgress, Container, Toolbar, Typography } from "@mui/material";
 
 const StationInfo = () => {
   const { stationCode } = useParams<"stationCode">();
@@ -10,22 +11,26 @@ const StationInfo = () => {
 
   if(station.isLoading){
     return (
-      <>
-        <p>Loading...</p>
-      </>
+      <Container>
+        <Typography variant="h6">Loading...</Typography>
+        <CircularProgress />
+      </Container>
     );
   }
 
   return (
     <>
-      <p>StationInfo</p>
-      <div>
-        <p>stationCode: {info?.stationCode}</p>
-        <p>railwayName: {info?.railwayName}</p>
-        <p>railwayCompany: {info?.railwayCompany}</p>
-        <p>latitude: {info?.latitude}</p>
-        <p>longitude: {info?.longitude}</p>
-      </div>
+      <Toolbar />
+      <Container>
+        <Typography variant="h4">{info?.stationName}</Typography>
+        <Box>
+          <Typography variant="h6">駅コード: {info?.stationCode}</Typography>
+          <p>路線名: {info?.railwayName}</p>
+          <p>路線運営会社: {info?.railwayCompany}</p>
+          <p>緯度: {info?.latitude}</p>
+          <p>経度: {info?.longitude}</p>
+        </Box>
+      </Container>
     </>
   );
 };
