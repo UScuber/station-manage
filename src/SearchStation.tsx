08520e7754a2b356488a28c5668aa4efc7606914
sota <sota1210nittc@gmail.com>
2023-12-14 08:57:06 +0900

    SearchStationに位置情報取得プログラムの追加

--- src/SearchStation.tsx
+++ src/SearchStation.tsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Coordinate } from "./Api";
+import { Box, Button, CircularProgress, Container, Typography } from "@mui/material";
 
 const SearchStation = () => {
+  const [isAvailable, setAvailable] = useState(false);
+  const [position, setPosition] = useState<Coordinate>();
+  const isFirstRef = useRef(true);
+
+  useEffect(() => {
+    isFirstRef.current = false;
+    if("geolocation" in navigator){
+      setAvailable(true);
+    }
+  }, [isAvailable]);
+
+  const getCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      setPosition({ lat: latitude, lng: longitude });
+    });
+  };
+
+  if(isFirstRef.current){
+    return (
+      <Container>
+        Loading...
+        <CircularProgress />
+      </Container>
+    );
+  }
+
   return (
-    <>
-      <p>SearchStation</p>
-    </>
+    <Container>
+      {!isAvailable && <p>Geolocation is not available.</p>}
+      {isAvailable && (
+        <Box>
+          <Button onClick={() => getCurrentPosition()}>Search</Button>
+          <Typography variant="h6">緯度: {position?.lat}</Typography>
+          <Typography variant="h6">経度: {position?.lng}</Typography>
+        </Box>
+      )}
+    </Container>
   );
 };
 
