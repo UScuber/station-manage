6a0554e5cddaa4f80512e5e5dc2b47da0b2f2c1e
sota <sota1210nittc@gmail.com>
2023-12-22 14:52:22 +0900

    座標から複数駅を表示するようにする

--- src/SearchStation.tsx
+++ src/SearchStation.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { Coordinate, useSearchNearestStationGroup, useStationsInfoByGroupCode } from "./Api";
+import { Coordinate, useSearchKNearestStationGroups, useStationsInfoByGroupCode } from "./Api";
 import {
   Box,
   Button,
@@ -9,16 +9,54 @@ import {
   Typography
 } from "@mui/material";
 
+type Props = {
+  code: number,
+  distance: number | undefined,
+};
+
+const StationGroupInfo: React.FC<Props> = (props) => {
+  const { code, distance } = props;
+  const stations = useStationsInfoByGroupCode(code);
+  const infos = stations.data;
+
+  if(stations.isLoading){
+    return (
+      <Box sx={{ mb: 2 }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  return (
+    <Box sx={{ mb: 4 }}>
+      {distance && (
+        <Typography sx={{ fontSize: 18 }}>距離: {distance.toFixed(3)}[km]</Typography>
+      )}
+      {infos?.map(station => (
+        <Button
+          component={Link}
+          to={"/station/" + station.stationCode}
+          variant="outlined"
+          color="inherit"
+          key={station.stationCode}
+          sx={{ display: "block", mb: 1, textTransform: "none" }}
+        >
+          <Typography variant="h6">駅名: {station.stationName}</Typography>
+          <Typography variant="h6">路線名: {station.railwayName}</Typography>
+          <Typography variant="h6">路線運営会社: {station.railwayCompany}</Typography>
+        </Button>
+      ))}
+    </Box>
+  );
+};
+
 const SearchStation = () => {
   const [isAvailable, setAvailable] = useState(false);
   const [position, setPosition] = useState<Coordinate>();
   const isFirstRef = useRef(true);
 
-  const nearestStationGroup = useSearchNearestStationGroup(position);
-  const groupStation = nearestStationGroup.data;
-
-  const stations = useStationsInfoByGroupCode(groupStation?.stationGroupCode);
-  const infos = stations.data;
+  const nearestStationGroups = useSearchKNearestStationGroups(position, 5);
+  const groupStations = nearestStationGroups.data;
 
   const getCurrentPosition = () => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -35,7 +73,7 @@ const SearchStation = () => {
     }
   }, []);
 
-  if(isFirstRef.current || nearestStationGroup.isLoading || stations.isLoading){
+  if(isFirstRef.current || nearestStationGroups.isLoading){
     return (
       <Container>
         Loading...
@@ -55,19 +93,12 @@ const SearchStation = () => {
         </Box>
       )}
       <Typography variant="h6">List</Typography>
-      {infos?.map((station, index) => (
-        <Button
-          component={Link}
-          to={"/station/" + station.stationCode}
-          variant="outlined"
-          color="inherit"
-          key={index}
-          sx={{ display: "block", mb: 2, textTransform: "none" }}
-        >
-          <Typography variant="h6">駅名: {station.stationName}</Typography>
-          <Typography variant="h6">路線名: {station.railwayName}</Typography>
-          <Typography variant="h6">路線運営会社: {station.railwayCompany}</Typography>
-        </Button>
+      {groupStations?.map(item => (
+        <StationGroupInfo
+          key={item.stationGroupCode}
+          code={item.stationGroupCode}
+          distance={item.distance}
+        />
       ))}
     </Container>
   );
