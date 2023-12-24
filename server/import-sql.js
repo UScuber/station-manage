9bc4c8e289598c4a6aee831be1d1d99f20538331
sota <sota1210nittc@gmail.com>
2023-12-25 00:25:13 +0900

    importのプログラムを一部だけ作成

--- server/import-sql.js
+++ server/import-sql.js
@@ -1,7 +1,7 @@
 // convert-geojson.js実行後のSQLに入れる
 
 const fs = require("fs");
-const sqlite3 = require("sqlite3");
+const sqlite3 = require("better-sqlite3");
 
 if(process.argv.length <= 2){
   console.error("Argument Error: node import-sql.js [json-file-name]");
@@ -17,7 +17,8 @@ if(!fs.existsSync("./station.db")){
   process.exit(1);
 }
 
-const db = new sqlite3.Database("./station.db");
+const db = sqlite3("./station.db");
+db.pragma("journal_mode = WAL");
 
 const input_json = JSON.parse(fs.readFileSync(file_path));
 
@@ -28,6 +29,23 @@ const distance = (p1, p2) => {
 };
 
 input_json.station_history.forEach(data => {
+  const res = db.prepare(`
+    SELECT * FROM Stations
+    INNER JOIN StationGroups
+      ON Stations.stationGroupCode = StationGroups.stationGroupCode
+        AND StationGroups.stationName = ?
+        AND Stations.railwayName = ?
+        AND Stations.railwayCompany = ?
+    `)
+    .get(data.info.stationName, data.info.railwayName, data.info.railwayCompany);
+  if(res){
+    data.history.forEach(elem => {
+      db.prepare("INSERT INTO StationHistory VALUES(?,datetime(?),?)")
+        .run(res.stationCode, elem.date, elem.state);
+    });
+  }else{
+    // 座標から探す
+  }
 });
 
 input_json.station_group_history.forEach(data => {
