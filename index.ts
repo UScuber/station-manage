00d6d1e3e598056fd0212f012bf4ec37a29f0126
sota <sota1210nittc@gmail.com>
2024-01-12 11:18:49 +0900

    index.tsの追加

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { serve } from "https://deno.land/std@0.188.0/http/server.ts";
+
+const contents = Array.from(Deno.readDirSync("build")).map(dir => "/" + dir.name);
+
+serve(
+  async(req: Request) => {
+    const path = new URL(req.url).pathname;
+    console.log(path);
+    const file_path = "build" + (contents.includes(path) || path.startsWith("/static") ? path : "/index.html");
+    const file = await Deno.open(file_path, { read: true });
+    return new Response(file.readable);
+  }
+);
