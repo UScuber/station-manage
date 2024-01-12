7745832060d52a21fd3535a49542d66081448145
sota <sota1210nittc@gmail.com>
2024-01-12 14:54:02 +0900

    jsonの読み込みと都道府県の登録

--- server/convert-geojson.js
+++ server/convert-geojson.js
@@ -1,16 +1,23 @@
 const fs = require("fs");
 const sqlite3 = require("better-sqlite3");
 
-if(process.argv.length <= 2){
-  console.error("Argument Error: node convert-geojson.js [geojson-file-name]");
+if(process.argv.length <= 3){
+  console.error("Argument Error: node convert-geojson.js [geojson-file-name] [json-station-name]");
   process.exit(1);
 }
-const file_path = process.argv[2];
-if(!fs.existsSync(file_path)){
-  console.error(`Error: ${file_path} does not exist`);
+const geojson_file_path = process.argv[2];
+if(!fs.existsSync(geojson_file_path)){
+  console.error(`Error: ${geojson_file_path} does not exist`);
+  process.exit(1);
+}
+const station_name_file_path = process.argv[3];
+if(!fs.existsSync(station_name_file_path)){
+  console.error(`Error: ${station_name_file_path} does not exist`);
   process.exit(1);
 }
 
+
+// geojson
 
 const max_same_station_dist = 0.45; // [km]
 
@@ -20,10 +27,10 @@ const distance = (p1, p2) => {
   return Math.acos(Math.cos(p1[0]*R) * Math.cos(p2[0]*R) * Math.cos(p2[1]*R - p1[1]*R) + Math.sin(p1[0]*R) * Math.sin(p2[0]*R)) * 6371;
 };
 
-let json_data = JSON.parse(fs.readFileSync(file_path).toString());
+let json_data = JSON.parse(fs.readFileSync(geojson_file_path).toString());
 json_data = json_data.features;
 
-json_data = json_data.map((elem) => {
+json_data = json_data.map(elem => {
   delete elem.type;
   elem["coordinates"] = [elem.geometry.coordinates]; // 駅の座標の線分
   delete elem.geometry;
@@ -54,10 +61,10 @@ json_data.forEach((elem, index) => {
     station_code_map[codes] = index;
   }
 });
-json_data = json_data.filter((elem) => elem !== null);
+json_data = json_data.filter(elem => elem !== null);
 
 // 重心を求める
-json_data = json_data.map((elem) => {
+json_data = json_data.map(elem => {
   let latitude = 0, longitude = 0;
   elem.coordinates.forEach((pos) => {
     // [経度, 緯度]
@@ -124,6 +131,31 @@ centers = centers.map((pos) => {
 });
 
 
+
+// station name
+
+let station_name_data = JSON.parse(fs.readFileSync(station_name_file_path));
+station_name_data = station_name_data.map(elem => ({
+  name: elem.original_name,
+  name_kana: elem.name_kana,
+  lat: elem.lat,
+  lng: elem.lng,
+  pref: elem.prefecture,
+}));
+
+let station_name_maps = {};
+station_name_data.forEach(elem => {
+  if(!(elem.name in station_name_maps)){
+    station_name_maps[elem.name] = [];
+  }
+  station_name_maps[elem.name].push(elem);
+});
+
+station_group_codes.forEach(code => {
+  const name = json_data[code].stationName;
+});
+
+
 if(fs.existsSync("./station.db")) fs.rmSync("./station.db");
 
 const db = sqlite3("./station.db");
@@ -148,6 +180,18 @@ const institution_type_cd_data = [
   { code: 3, content: "公営鉄道" },
   { code: 4, content: "民営鉄道" },
   { code: 5, content: "第三セクター" },
+];
+const prefecture = [
+  { code: 1, name: "北海道" },
+  { code: 2, name: "青森県" }, { code: 3, name: "岩手県" }, { code: 4, name: "宮城県" }, { code: 5, name: "秋田県" }, { code: 6, name: "山形県" }, { code: 7, name: "福島県" },
+  { code: 8, name: "茨城県" }, { code: 9, name: "栃木県" }, { code: 10, name: "群馬県" }, { code: 11, name: "埼玉県" }, { code: 12, name: "千葉県" }, { code: 13, name: "東京都" }, { code: 14, name: "神奈川県" },
+  { code: 15, name: "新潟県" }, { code: 16, name: "富山県" }, { code: 17, name: "石川県" }, { code: 18, name: "福井県" }, { code: 19, name: "山梨県" }, { code: 20, name: "長野県" },
+  { code: 21, name: "岐阜県" }, { code: 22, name: "静岡県" }, { code: 23, name: "愛知県" }, { code: 24, name: "三重県" },
+  { code: 25, name: "滋賀県" }, { code: 26, name: "京都府" }, { code: 27, name: "大阪府" }, { code: 28, name: "兵庫県" }, { code: 29, name: "奈良県" }, { code: 30, name: "和歌山県" },
+  { code: 31, name: "鳥取県" }, { code: 32, name: "島根県" }, { code: 33, name: "岡山県" }, { code: 34, name: "広島県" }, { code: 35, name: "山口県" },
+  { code: 36, name: "徳島県" }, { code: 37, name: "香川県" }, { code: 38, name: "愛媛県" }, { code: 39, name: "高知県" },
+  { code: 40, name: "福岡県" },{ code: 41, name: "佐賀県" }, { code: 42, name: "長崎県" }, { code: 43, name: "熊本県" }, { code: 44, name: "大分県" }, { code: 45, name: "宮崎県" }, { code: 46, name: "鹿児島県" },
+  { code: 47, name: "沖縄県" },
 ];
 
 console.log("Create tables");
@@ -176,6 +220,18 @@ db.transaction(() => {
   `).run();
   institution_type_cd_data.forEach(data => {
     db.prepare("INSERT INTO InstitutionTypeCd VALUES(?,?)").run(data.code, data.content);
+  });
+
+  // Prefectures
+  db.prepare(`
+  CREATE TABLE Prefectures(
+    prefCode INTEGER,
+    prefName VARCHAR(4),
+    PRIMARY KEY (prefCode)
+  )
+  `).run();
+  prefecture.forEach(data => {
+    db.prepare("INSERT INTO Prefectures VALUES(?,?)").run(data.code, data.name);
   });
 
   // StationGroups
