649ec5faa3d1410ca6e5041a3a68c4c09fb6fe87
sota <sota1210nittc@gmail.com>
2024-01-25 17:15:56 +0900

    StationGroupに履歴を表示する表を追加

--- src/StationGroupInfo.tsx
+++ src/StationGroupInfo.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Box, Container } from "@mui/system";
-import { Button, CircularProgress, Typography } from "@mui/material";
+import { Button, CircularProgress, Collapse, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
+import { KeyboardArrowUp as KeyboardArrowUpIcon, KeyboardArrowDown as KeyboardArrowDownIcon } from "@mui/icons-material";
 import { Station, StationGroup, useSendStationGroupStateMutation, useStationGroupInfo, useStationsInfoByGroupCode } from "./Api";
 import AccessButton from "./components/AccessButton";
+import AroundTime from "./components/AroundTime";
 
 
 const StationItem = (
@@ -18,13 +20,13 @@ const StationItem = (
       to={"/station/" + info.stationCode}
       variant="outlined"
       color="inherit"
-      sx={{ display: "block", mb: 3, textTransform: "none" }}
+      sx={{ display: "block", mb: 0.5, textTransform: "none" }}
     >
       <Typography variant="h6" sx={{ fontSize: 15, display: "inline-block" }}>{info?.railwayCompany}</Typography>
       <Typography variant="h6" sx={{ mx: 1, display: "inline-block" }}>{info?.railwayName}</Typography>
 
-      <Typography variant="h6" sx={{ fontSize: 18 }}>乗降: {info?.getDate?.toString() ?? "なし"}</Typography>
-      <Typography variant="h6" sx={{ fontSize: 18 }}>通過: {info?.passDate?.toString() ?? "なし"}</Typography>
+      <Typography variant="h6" sx={{ fontSize: 18 }}>乗降: <AroundTime date={info?.getDate?.toString() ?? "なし"}/></Typography>
+      <Typography variant="h6" sx={{ fontSize: 18 }}>通過: <AroundTime date={info?.passDate?.toString() ?? "なし"}/></Typography>
     </Button>
   );
 };
@@ -33,6 +35,7 @@ const StationGroupInfo = () => {
   const stationGroupCode = Number(useParams<"stationGroupCode">().stationGroupCode);
 
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const groupStations = useStationsInfoByGroupCode(stationGroupCode);
   const stationList = groupStations.data;
@@ -83,17 +86,46 @@ const StationGroupInfo = () => {
         </Box>
           <Typography variant="h6">{groupStationData?.prefName}</Typography>
         </Box>
-        <Typography variant="h6">立ち寄り: {groupStationData?.date?.toString() ?? "なし"}</Typography>
+        <Typography variant="h6" sx={{ display: "inline-block" }}>
+          立ち寄り: <AroundTime date={groupStationData?.date?.toString() ?? "なし"}/>
+        </Typography>
+        <IconButton
+          aria-label="expand row"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </IconButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <Box sx={{ margin: 1 }}>
+            <Typography variant="h6" component="div">
+              History
+            </Typography>
+            <Table size="small" aria-label="dates">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {groupStationData?.date && (
+                  <TableRow>
+                    <TableCell>{groupStationData?.date?.toString() ?? ""}</TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </Box>
+        </Collapse>
         <AccessButton
           text="立ち寄り"
           loading={loading}
           timeLimit={60*3}
           accessedTime={groupStationData?.date?.toString()}
           onClick={handleSubmit}
-          sx={{ mb: 2 }}
+          sx={{ mb: 2, display: "block" }}
         />
       <Box>
-        {stationList?.map((item) => (
+        {stationList?.map(item => (
           <StationItem key={item.stationCode} info={item} />
         ))}
       </Box>
--- src/components/AroundTime.tsx
+++ src/components/AroundTime.tsx
@@ -26,7 +26,7 @@ const getAroundTime = (date: Date | string): string => {
 
 const getDateString = (date: Date, disableMinute: boolean | undefined): string => {
   return `${date.getFullYear()}-${("0"+date.getMonth()+1).slice(-2)}-${("0"+date.getDate()).slice(-2)} ${("0"+date.getHours()).slice(-2)}:${("0"+date.getMinutes()).slice(-2)}`
-    + (disableMinute ? "" : ("0"+date.getSeconds()).slice(-2));
+    + (disableMinute ? "" : ":" + ("0"+date.getSeconds()).slice(-2));
 };
 
 const AroundTime = (
