1ab053f8fdead32b9bbfe5338733e93afccd6340
sota <sota1210nittc@gmail.com>
2024-03-11 19:51:19 +0900

    fontの変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { CssBaseline, Toolbar, styled } from "@mui/material";
+import { CssBaseline, ThemeProvider, Toolbar, createTheme, styled } from "@mui/material";
 import Header from "./Header";
 import Top from "./Top";
 import StationList from "./StationList";
@@ -14,6 +14,20 @@ import CompanyInfo from "./CompanyInfo";
 import HistoryMap from "./HistoryMap";
 import PrefectureInfo from "./PrefectureInfo";
 
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '"Hiragino Kaku Gothic Pro"',
+      '"Hiragino Sans"',
+      "arial",
+      "Meiryo",
+      '"MS PGothic"',
+      "sans-serif",
+    ].join(","),
+  },
+});
+
 const ThinToolbar = styled(Toolbar)(({ theme }) => ({
   minHeight: 25,
 }));
@@ -23,24 +37,26 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Header />
-        <ThinToolbar />
-        <Routes>
-          <Route path="/" element={<Top />} />
-          <Route path="/stationList" element={<StationList />} />
-          <Route path="/station/:stationCode" element={<StationInfo />} />
-          <Route path="/stationGroup/:stationGroupCode" element={<StationGroupInfo />} />
-          <Route path="/railway/:railwayCode" element={<RailwayInfo />} />
-          <Route path="/company/:companyCode" element={<CompanyInfo />} />
-          <Route path="/pref/:prefCode" element={<PrefectureInfo />} />
-          <Route path="/searchStation" element={<SearchStation />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/historyMap" element={<HistoryMap />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Header />
+          <ThinToolbar />
+          <Routes>
+            <Route path="/" element={<Top />} />
+            <Route path="/stationList" element={<StationList />} />
+            <Route path="/station/:stationCode" element={<StationInfo />} />
+            <Route path="/stationGroup/:stationGroupCode" element={<StationGroupInfo />} />
+            <Route path="/railway/:railwayCode" element={<RailwayInfo />} />
+            <Route path="/company/:companyCode" element={<CompanyInfo />} />
+            <Route path="/pref/:prefCode" element={<PrefectureInfo />} />
+            <Route path="/searchStation" element={<SearchStation />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/historyMap" element={<HistoryMap />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 };
