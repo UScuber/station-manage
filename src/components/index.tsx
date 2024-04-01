78297a88ede7a30d82e15f86c2c12e91259dd6fd
sota <sota1210nittc@gmail.com>
2024-04-02 01:14:25 +0900

    ConfirmDialogを追加

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as AccessButton } from "./AccessButton";
 export { default as AroundTime } from "./AroundTime";
 export { default as BinaryPagination } from "./BinaryPagination";
+export { default as ConfirmDialog } from "./ConfirmDialog";
 export { default as CustomLink } from "./CustomLink";
 export { default as RespStationName } from "./RespStationName";
