e9c98a1814f177424482013b4686aa6826986d80
sota <sota1210nittc@gmail.com>
2024-04-25 19:35:42 +0900

    vite.config.ts追加

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  server: {
+    host: true,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "./src/"),
+    },
+  },
+  build: {
+    outDir: "build",
+  },
+  plugins: [react()],
+});
