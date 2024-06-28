3d60718deffdd6f2d195b0125fd1e0dc3197a165
sota <sota1210nittc@gmail.com>
2024-06-29 07:07:29 +0900

    index.tsでのファイルのアップロード方法を変更(hotfix)

--- index.ts
+++ index.ts
@@ -1,12 +1,23 @@
 import { serve } from "https://deno.land/std@0.188.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+
 
 const contents = Array.from(Deno.readDirSync("build")).map(dir => "/" + dir.name);
+const HTML = await Deno.readFile("./build/index.html");
 
 serve(
   async(req: Request) => {
     const path = new URL(req.url).pathname;
-    const file_path = "build" + (contents.includes(path) || path.startsWith("/assets") ? path : "/index.html");
-    const file = await Deno.open(file_path, { read: true });
-    return new Response(file.readable);
+
+    if(contents.includes(path) || path.startsWith("/assets")){
+      return serveDir(req, {
+        fsRoot: "build",
+      });
+    }
+    return new Response(HTML, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    })
   }
 );
