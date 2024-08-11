bf65dcadd6d06e315987fd5bae0cd79ddc2f9431
sota <sota1210nittc@gmail.com>
2024-08-12 00:00:28 +0900

    履歴の経路を一部省略して表示

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -15,9 +15,9 @@ import {
   Typography,
 } from "@mui/material";
 import { Search as SearchIcon } from "@mui/icons-material";
-import { useStationHistoryList, useStationHistoryCount, StationHistoryDetail } from "../api";
+import { useStationHistoryList, useStationHistoryCount, StationHistoryDetail, RecordState } from "../api";
 import { useAuth } from "../auth";
-import { BinaryPagination, CustomLink, RespStationName } from "../components";
+import { BinaryPagination, Collapser, CustomLink, RespStationName } from "../components";
 import getDateString from "../utils/getDateString";
 import NotFound from "./NotFound";
 import aroundDayName from "../utils/aroundDayName";
@@ -54,10 +54,70 @@ const HistoryContent = (
 };
 
 
+const OmittedContents = (
+  { historyList }
+  :{
+    historyList: StationHistoryDetail[]
+  }
+) => {
+  if(historyList.length === 0){
+    return (
+      <></>
+    );
+  }
+
+  if(historyList.length === 1){
+    return (
+      <Box sx={{ ml: 2 }}>
+        <HistoryContent history={historyList[0]} />
+      </Box>
+    );
+  }
+
+  return (
+    <Collapser
+      buttonText={
+        <Typography variant="h6" sx={{ display: "inline" }}>
+          | {historyList.length}駅
+        </Typography>
+      }
+      sx={{ ml: 3 }}
+    >
+      {historyList.map(history => (
+        <Box sx={{ ml: 2 }} key={`${history.date}|${history.state}|${history.stationCode}`}>
+          <HistoryContent history={history} />
+        </Box>
+      ))}
+    </Collapser>
+  );
+};
+
+
+// 詳細で表示するものとそのまま表示するものを分ける
+// そのまま表示する要素のindexを返す
+const splitHistoryList = (historyList: StationHistoryDetail[]): (StationHistoryDetail & { idx: number })[] => {
+  if(historyList.length <= 3){
+    return historyList.map((history, idx) => ({ ...history, idx: idx }));
+  }
+
+  let res: (StationHistoryDetail & { idx: number })[] = [{ ...historyList[0], idx: 0 }];
+  for(let i = 1; i < historyList.length-1; i++){
+    const history = historyList[i];
+    if(history.state === RecordState.Get
+      || historyList[i-1].railwayCode !== history.railwayCode
+      || historyList[i+1].railwayCode !== history.railwayCode
+      || historyList[i-1].date.getTime() - history.date.getTime() >= 1000*60*60*24
+      || history.date.getTime() - historyList[i+1].date.getTime() >= 1000*60*60*24) res.push({ ...history, idx: i });
+  }
+  res.push({ ...historyList[historyList.length-1], idx: historyList.length - 1 });
+  return res;
+};
+
+
 const History = () => {
   const { isAuthenticated } = useAuth();
   const [page, setPage] = useState(1);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rowsPerPage, setRowsPerPage] = useState(50);
   const [timeoutId, setTimeoutId] = useState<NodeJS.Timer>();
   const [inputName, setInputName] = useState("");
   const [searchName, setSearchName] = useState("");
@@ -98,7 +158,7 @@ const History = () => {
         page={page}
         count={historyListCount.data!}
         rowsPerPage={rowsPerPage}
-        rowsPerPageOptions={[10,25,50,100]}
+        rowsPerPageOptions={[20,50,100,200,1000]}
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
         sx={{ my: 1 }}
@@ -207,11 +267,13 @@ const History = () => {
       </Box>
 
       <Box>
-        {historyList.data.map((item, index, list) => {
+        {splitHistoryList(historyList.data).map((item, i, list) => {
           const date = item.date;
-          const isSameDate = index && list[index-1].date.getTime() - date.getTime() < 1000*60*60*24;
+          const isSameDate = i && list[i-1].date.getTime() - date.getTime() < 1000*60*60*24;
           return (
             <Box key={`${date.toString()}|${item.stationCode}|${item.state}`}>
+              {/* 省略 */}
+              {!!isSameDate && (<OmittedContents historyList={historyList.data.slice(list[i-1].idx+1, item.idx)}/>)}
               {/* 日付 */}
               {!isSameDate && (
                 <Typography variant="h6" sx={{ mt: 1 }}>
@@ -220,7 +282,7 @@ const History = () => {
                 </Typography>
               )}
               {/* 路線名 */}
-              {(!isSameDate || item.railwayCode !== list[index-1].railwayCode) && (
+              {(!isSameDate || item.railwayCode !== list[i-1].railwayCode) && (
                 <Box sx={{ ml: 1 }}>
                   <Button
                     component={Link}
